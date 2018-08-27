564969bae120bb0b356efcb6ae2ce3aaebec1619
0x4139 <0x4139@gmail.com>
2018-08-27 14:36:53 +0300

    header location for cl on linux

--- cl.go
+++ cl.go
@@ -15,9 +15,9 @@ to avoid leaks. This is similar to how file handles and such are handled
 in the Go standard packages.
 */
 package cl
-// #cgo CFLAGS: -I/usr/include/CL
+// #cgo CFLAGS: -I/usr/include
 // #cgo darwin LDFLAGS: -framework OpenCL
-// #cgo windows LDFLAGS: -lOpenCL
+// #cgo linux LDFLAGS: -L/usr/include
 import "C"
 import "errors"
 
